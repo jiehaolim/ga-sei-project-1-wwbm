e7608d5cb3b3b96d5626f20f8c37cf877366f0b3
limjiehao <jiehao15@gmail.com>
2022-08-22 15:28:07 +0800

    56th commit - updated

--- main.js
+++ main.js
@@ -623,7 +623,6 @@ const $restartGame = () => {
 
 // document ready!
 $(() => {
-  $("#music").get(0).play()
   $(".menu").eq(0).on("click", $startGame);
   $(".menu").eq(1).on("click", $rules);
   $(".menu").eq(2).on("click", $displayScoreboard);
